93e18992e0941fdd7caf4b51bdc4dc034b5a543e
HaziimYassien <haziim.yassien@gmail.com>
2023-01-09 01:27:32 +0200

    gfdg

--- Section_3_react_basics/second_first_component/second_first_componentt/src/Components/FirstCompnent.js
+++ Section_3_react_basics/second_first_component/second_first_componentt/src/Components/FirstCompnent.js
@@ -1,10 +1,26 @@
-function FirstComponent() {
-    return 
-    <div>
-      <h1> this is an h1 inside the div inside th component </h1>
-      <h2> this is an h2 inside hte dive inside the component</h2>
-    </div>
+import "./FirstComponent.css";
 
+function FirstComponent() {
+  const gameDateRelease = new Date(2017, 2, 22);
+  const BetaGameDateRelease = new Date(2014, 2, 21);
+  const gameRating = "Meh";
+  const gamePrice = 2222.22;
+  return (
+    <div className="main_div">
+      <h1 className="lol_player">
+        hahaaaa this dude plays lol he muse be depressed
+      </h1>
+      <h1 className="valorant_player">
+        this dude plays valornat he must have alot of friends
+      </h1>
+      <h1 className="otherStuff">
+        this dude plays a differnet game we don't care
+      </h1>
+      <h2>{gameDateRelease.toISOString()}</h2>
+      <h2>{gameRating}</h2>
+      <h2>{gamePrice}</h2>
+    </div>
+  );
 }
 
-export default FirstComponent
+export default FirstComponent;
