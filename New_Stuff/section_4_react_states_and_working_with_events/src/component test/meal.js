017f0203a1fa5253b95b6bce70d146b133539af7
HaziimYassien <haziim.yassien@gmail.com>
2023-01-17 02:33:47 +0200

    stuff

---
+++ New_Stuff/section_4_react_states_and_working_with_events/src/component test/meal.js
@@ -0,0 +1,13 @@
+function tomato() {
+  return <p>tomato</p>;
+}
+function beans() {
+  return <p>beans</p>;
+}
+function onion() {
+  return <p>onion</p>;
+}
+
+export default function meal() {
+  return <p>`how to make cool beans first add the ${onion}`</p>;
+}
